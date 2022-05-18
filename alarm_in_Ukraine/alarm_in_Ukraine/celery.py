c02ddaf3ea28e4bd0e3d515cf4ece1ce7b515fc2
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-18 20:54:05 +0300

    feat(module): add celery - add celery module - add tasks module - add celery settings to settings of project - add celery settings to __init__.py

---
+++ alarm_in_Ukraine/alarm_in_Ukraine/celery.py
@@ -0,0 +1,27 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alarm_in_Ukraine.settings')
+app = Celery('alarms')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'set_alarms_to_db': {
+        'task': 'alarms.tasks.set_alarms_to_db',
+        'schedule': 300.0
+    },
+    'set_states_to_db': {
+        'task': 'alarms.tasks.set_states_to_db',
+        'schedule': 86400.0
+    },
+
+}
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
