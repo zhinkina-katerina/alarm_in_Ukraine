0f39a2cc060c362f8954fd32c33eb6c20b91aa7c
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-18 21:05:10 +0300

    features(module): add checks database filling - add check for city database existence - add check of last added alarm - fix state_id field in set_alarms

--- alarm_in_Ukraine/alarms/data_base_provider.py
+++ alarm_in_Ukraine/alarms/data_base_provider.py
@@ -1,22 +1,30 @@
-from alerts_connector import AlertsConnector
-from models import States, Alarm
+from .alerts_connector import AlertsConnector
+from .models import States, Alarm
+from django.db.models import Max
 
 
 class DataBaseProvider:
     def set_states(self, response):
+        if States.objects.first():
+            return
         for item in response['states']:
-            id = item.get('id')
-            name = item.get('name')
-            States.objects.create(id=id,
-                                  name=name)
+            id_item = item.get('id')
+            state = item.get('name')
+            States.objects.create(id=id_item,
+                                  state=state)
 
     def set_alarms(self, response):
-        for item in response:
-            id = item.get('id')
+        max_id = Alarm.objects.all().aggregate(Max('id')).get('id__max')
+        new_alarms = response[max_id:]
+        for item in new_alarms:
+            id_item = item.get('id')
             date = item.get('date')
-            state_id = item.get('state_id')
+
+            state_id = int(item.get('state_id'))
+            state_id = States.objects.only('id').get(id=state_id)
+
             alert = item.get('alert')
-            Alarm.objects.create(id=id,
+            Alarm.objects.create(id=id_item,
                                  date=date,
                                  state_id=state_id,
                                  alert=alert)
