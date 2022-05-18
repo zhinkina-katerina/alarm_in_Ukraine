c277776ea955ddeebf8882401100a6bbe51434fd
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-18 14:22:13 +0300

    feat(model): add models - add model States - add model Alarm

--- alarm_in_Ukraine/alarms/models.py
+++ alarm_in_Ukraine/alarms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class States(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    state = models.CharField(max_length=200)
+
+
+class Alarm(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    date = models.DateField()
+    alert = models.BooleanField()
+    state_id = models.ForeignKey(States, on_delete=models.CASCADE)
