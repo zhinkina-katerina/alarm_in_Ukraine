7623ec251d671965d049c376dded2050c669ecd4
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-20 22:55:34 +0300

    fix():edit celery.py - add app.conf.update

--- alarm_in_Ukraine/alarm_in_Ukraine/celery.py
+++ alarm_in_Ukraine/alarm_in_Ukraine/celery.py
@@ -1,11 +1,16 @@
 import os
 
 from celery import Celery
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alarm_in_Ukraine.settings')
 app = Celery('alarms')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 app.autodiscover_tasks()
 
