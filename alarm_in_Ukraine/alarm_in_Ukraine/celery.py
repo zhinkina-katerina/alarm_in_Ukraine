fa6130295294e11d269840a5b0e74cb86763f0c7
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-20 23:25:43 +0300

    fix():edit celery.py - edit app.conf.update - edit Procfile

--- alarm_in_Ukraine/alarm_in_Ukraine/celery.py
+++ alarm_in_Ukraine/alarm_in_Ukraine/celery.py
@@ -8,9 +8,11 @@ app = Celery('alarms')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+app.conf.update(
+     CELERY_ACCEPT_CONTENT = ['json'],
+     CELERY_TASK_SERIALIZER = 'json',
+     CELERY_RESULT_SERIALIZER = 'json',
+)
 
 app.autodiscover_tasks()
 
